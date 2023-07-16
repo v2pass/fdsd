647a75295904cd85ebec71c4b69ba81712fed57c
idkeosled <25784252+v2pass@users.noreply.github.com>
2023-07-16 20:47:55 +0800

    Update download_geoFiles.sh

--- download_geoFiles.sh
+++ download_geoFiles.sh
@@ -4,27 +4,34 @@
 geoip_url="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 geosite_url="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 
-# 下载 geoip.dat 文件
-wget -O /etc/XrayR/geoip.dat $geoip_url
+# 下载文件函数
+download_file() {
+    local url=$1
+    local output=$2
+    local max_retries=10
+    local retries=0
 
-# 检查下载是否成功
-if [ $? -eq 0 ]; then
-    echo "geoip.dat 下载成功！"
-else
-    echo "geoip.dat 下载失败！"
-    exit 1
-fi
+    while [ $retries -lt $max_retries ]; do
+        wget -O $output $url
+
+        if [ $? -eq 0 ]; then
+            echo "$output 下载成功！"
+            return 0
+        else
+            echo "$output 下载失败，重试中..."
+            retries=$((retries+1))
+        fi
+    done
+
+    echo "$output 下载失败！"
+    return 1
+}
+
+# 下载 geoip.dat 文件
+download_file $geoip_url "/etc/XrayR/geoip.dat"
 
 # 下载 geosite.dat 文件
-wget -O /etc/XrayR/geosite.dat $geosite_url
-
-# 检查下载是否成功
-if [ $? -eq 0 ]; then
-    echo "geosite.dat 下载成功！"
-else
-    echo "geosite.dat 下载失败！"
-    exit 1
-fi
+download_file $geosite_url "/etc/XrayR/geosite.dat"
 
 # 脚本执行完毕
 echo "文件下载完成！"
